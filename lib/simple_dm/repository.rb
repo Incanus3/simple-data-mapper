1bfd2bb8c9f3042bf6ec4882e84d6b38f00b9066
Jakub Kaláb <jakubkalab@gmail.com>
2019-12-03 04:44:49 +0100

    split into separate files

--- lib/simple_dm/repository.rb
+++ lib/simple_dm/repository.rb
@@ -1,10 +1,13 @@
 require_relative './utils'
+require_relative './relation'
 
 # TODO: add mappers
 # TODO: add validations
 
 module SimpleDM
   class Backend
+    @default_registered_name = nil
+
     class << self
       def default_registered_name
         @default_registered_name or raise 'default registered name not provided'
@@ -79,74 +82,6 @@ module SimpleDM
     private
 
     attr_reader :backend
-  end
-
-
-  class Relation
-    class << self
-      def inherited(relation_class)
-        relation_class.const_set('Dataset', Class.new(Dataset))
-      end
-
-      def dataset_class
-        const_get('Dataset')
-      end
-
-      def schema(&block)
-        builder = Internal::SchemaBuilder.new
-
-        builder.instance_eval(&block)
-      end
-    end
-
-    def initialize(repository, registered_name)
-      @repository      = repository
-      @registered_name = registered_name
-    end
-
-    def create(**attributes)
-      repository.store(registered_name, attributes)
-
-      attributes
-    end
-
-    def all
-      create_dataset(repository.fetch_all(registered_name))
-    end
-
-    def where(**filters)
-      create_dataset(repository.fetch_filtered(registered_name, **filters))
-    end
-
-    private
-
-    attr_reader :repository, :registered_name
-
-    def create_dataset(records)
-      self.class.dataset_class.new(records)
-    end
-  end
-
-
-  class Dataset
-    def initialize(records)
-      @records = records
-    end
-
-    def to_a
-      @records
-    end
-  end
-
-
-  module Internal
-    class SchemaBuilder
-      def primary_key(column_name)
-      end
-
-      def string(column_name, max_length: nil, unique: false)
-      end
-    end
   end
 end
 
